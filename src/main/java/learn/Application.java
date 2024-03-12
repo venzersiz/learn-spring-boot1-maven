bb793d1225ed74a23973666e0fd8264d0fe80ee4
Gangha Ryu <venatus@nhnpayco.com>
2024-03-12 09:54:05 +0900

    @SpringBootApplication 적용

--- src/main/java/learn/Application.java
+++ src/main/java/learn/Application.java
@@ -1,11 +1,11 @@
 package learn;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @RestController
 public class Application {
 
